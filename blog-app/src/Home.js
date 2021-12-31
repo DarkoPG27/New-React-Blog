529b0f660749204ee82401025f6f72727ef683f8
Darko <89515851+DarkoPG27@users.noreply.github.com>
2021-12-31 02:15:29 +0100

    Added blog preview

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: "Moj novi sajt", body: "dasdasdasdasdas", image: "https://materializecss.com/images/sample-1.jpg", author: "Me", id: 1 }
+    ])
+
     return (
         <div className="home">
             <h1>SVI POSTOVI</h1>
-        </div>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <img src={blog.image} alt={blog.image} />
+                    <h2>{blog.title}</h2>
+                    <p>Autor: {blog.author}</p>
+                </div>
+            ))}
+
+        </div >
     );
 }
 
