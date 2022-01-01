93d6ba11707c3ed9ec3ecdb51572de411616089d
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-01 22:01:36 +0100

    Added inputs on Create form

--- blog-app/src/Create.js
+++ blog-app/src/Create.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("");
+    const [url, setUrl] = useState("");
+
     return (
         <div className="create">
             <h2>Dodaj Novi Post</h2>
+            <form>
+                <label>Naslov:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Sadržaj:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                >
+                </textarea>
+                <label>URL fotografije:</label>
+                <input
+                    type="link"
+                    required
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                />
+                <label>Ime autora:</label>
+                <input
+                    type="text"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
+                <button>Dodaj post</button>
+            </form>
         </div>
     );
 }
