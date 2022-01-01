a7eda83a234d8f8a65e0966b8c4d0fdfcca3fbb7
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-01 17:10:58 +0100

    handling errors

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -2,24 +2,30 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState("");
+    const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-
+    const [error, setError] = useState(null)
     useEffect(() => {
         fetch('https://jsonblob.com/api/jsonBlob/926305735986659328')
             .then(response => {
+                if (!response.ok) {
+                    throw Error("could not fetch the data")
+                }
                 return response.json();
             })
             .then(response => {
                 setBlogs(response.blogs);
-                setIsLoading(false)
+                setIsLoading(false);
+                setError(null);
             })
-            .catch(function (error) {
-                console.log(error);
+            .catch(error => {
+                setIsLoading(false);
+                setError(error.message);
             });
     }, []);
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} />}
         </div >
