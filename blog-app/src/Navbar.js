66f1c4d2019ee3c92bd6bb5da5ed9b8bcd50c95b
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-01 18:30:58 +0100

    router links

--- blog-app/src/Navbar.js
+++ blog-app/src/Navbar.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="links">
-                <a href="/"><img className="logo" src="/logo.png"></img></a>
-                <a href="/add-post"> <button className="add-post">Dodaj post</button></a>
+                <Link to="/"><img className="logo" src="/logo.png"></img></Link>
+                <Link to="/add-post"> <button className="add-post">Dodaj post</button></Link>
             </div>
         </nav>
     );
